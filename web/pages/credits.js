10a2892f49a0fbfc3b9a028bd846aa0debb35d63
Dusty McCord <Dustymccord@gmail.com>
2020-08-19 23:10:30 -0700

    update styling for credits.

--- web/pages/credits.js
+++ web/pages/credits.js
@@ -12,58 +12,62 @@ function urlFor(source) {
 
 const credits = ({ crew, cast }) => {
   return (
-    <div className={s.credits}>
+    <>
       <PageTitle title={'Credits'} />
-      <h2>Creative & Production</h2>
-      <div className={s['credits-crew']}>
-        {crew.map((person) => (
-          <div className={s['credits-crew-member']}>
-            <div className={s['credits-crew-member-name']}>{person.name}</div>
-            <div className={s['credits-crew-member-body']}>
-              <div className={s['credits-crew-member-body-img']}>
-                <img src={urlFor(person.image)} />
-              </div>
-              <div className={s['credits-crew-member-body-info']}>
-                <BlockContent blocks={person.bio} {...client.config()} />
-              </div>
-            </div>
-            <div className={s['credits-crew-member-job']}>
-              {person.jobs.split(',').map((job) => (
-                <div className={s['credits-crew-member-job-item']}>{job}</div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
-      <h2>Cast</h2>
-      <div className={s['credits-cast']}>
-        {cast.map((person) => (
-          <div className={s['credits-cast-member']}>
-            <div className={s['credits-cast-member-name']}>{person.name}</div>
-            <div className={s['credits-cast-member-body']}>
-              <div className={s['credits-cast-member-body-img']}>
-                <img src={urlFor(person.image)} />
-              </div>
-              <div className={s['credits-cast-member-body-info']}>
-                <div className={s['credits-cast-member-body-info-voice']}>
-                  <h3>Voice for:</h3>
-                  {person.voiced.split(',').map((voice, index, array) => (
-                    <div
-                      className={s['credits-cast-member-body-info-voice-item']}>
-                      {' '}
-                      {voice} {index < array.length - 1 ? '|' : ''}
-                    </div>
-                  ))}
+      <div className={s.credits}>
+        <h2>Creative & Production</h2>
+        <div className={s['credits-crew']}>
+          {crew.map((person) => (
+            <div className={s['credits-crew-member']}>
+              <div className={s['credits-crew-member-name']}>{person.name}</div>
+              <div className={s['credits-crew-member-body']}>
+                <div className={s['credits-crew-member-body-img']}>
+                  <img src={urlFor(person.image)} />
                 </div>
-                <div className={s['credits-cast-member-body-info-bio']}>
+                <div className={s['credits-crew-member-body-info']}>
                   <BlockContent blocks={person.bio} {...client.config()} />
                 </div>
               </div>
+              <div className={s['credits-crew-member-job']}>
+                {person.jobs.split(',').map((job) => (
+                  <div className={s['credits-crew-member-job-item']}>{job}</div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <h2>Cast</h2>
+        <div className={s['credits-cast']}>
+          {cast.map((person) => (
+            <div className={s['credits-cast-member']}>
+              <div className={s['credits-cast-member-name']}>{person.name}</div>
+              <div className={s['credits-cast-member-body']}>
+                <div className={s['credits-cast-member-body-img']}>
+                  <img src={urlFor(person.image)} />
+                </div>
+                <div className={s['credits-cast-member-body-info']}>
+                  <div className={s['credits-cast-member-body-info-voice']}>
+                    <h3>Voice for:</h3>
+                    {person.voiced.split(',').map((voice, index, array) => (
+                      <div
+                        className={
+                          s['credits-cast-member-body-info-voice-item']
+                        }>
+                        {' '}
+                        {voice} {index < array.length - 1 ? '|' : ''}
+                      </div>
+                    ))}
+                  </div>
+                  <div className={s['credits-cast-member-body-info-bio']}>
+                    <BlockContent blocks={person.bio} {...client.config()} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
