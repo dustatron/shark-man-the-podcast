ed07820ac2158924d763a4b7c0670284b3d303d3
Dusty McCord <Dustymccord@gmail.com>
2020-08-21 10:02:14 -0700

    add contact bootstrap form.

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -1,10 +1,39 @@
 import s from '../sass/pages/contact.module.scss';
 import PageTitle from '../components/PageTitle';
+import { Form, Button } from 'react-bootstrap';
 
 const contact = () => {
   return (
     <div className={s.contact}>
       <PageTitle title={'Contact Us'} />
+
+      <div className={s['contact-form']}>
+        <Form>
+          <h2>Submit a comment</h2>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='Enter email' />
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' placeholder='Your Name' />
+          </Form.Group>
+
+          <Form.Group controlId='formBasicPassword'>
+            <Form.Label>Comment</Form.Label>
+            <Form.Control
+              as='textarea'
+              rows='10'
+              placeholder='Leave your message here'
+            />
+          </Form.Group>
+
+          <Button variant='light' size='lg' type='submit' block>
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
