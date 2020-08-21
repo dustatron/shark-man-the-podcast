129788619906b211ac22447c02eb2fd8a6d4a179
Dusty McCord <Dustymccord@gmail.com>
2020-08-20 17:49:50 -0700

    update style in credits and add admin page.

---
+++ web/pages/admin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const admin = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center', marginTop: '2rem' }}>Admin</h2>
+      <div style={{ textAlign: 'center', fontSize: '1.6rem' }}>
+        <a href='https://shark-man-the-podcast.sanity.studio/desk/'>
+          admin panel
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default admin;
