6eda84ac4f20cedcc908adea324f0146121792fb
Dusty McCord <Dustymccord@gmail.com>
2020-08-21 10:51:02 -0700

    fix mobile issues for contact form.

--- web/pages/contact.js
+++ web/pages/contact.js
@@ -1,45 +1,46 @@
 import s from '../sass/pages/contact.module.scss';
 import PageTitle from '../components/PageTitle';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 const contact = () => {
   return (
-    <div className={s.contact}>
+    <>
       <PageTitle title={'Contact Us'} />
+      <div className={s.contact}>
+        <div className={s['contact-form']}>
+          <Form>
+            <h2>Submit a comment</h2>
+            <Form.Group controlId='formBasicEmail'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type='email' placeholder='Enter email' />
+            </Form.Group>
 
-      <div className={s['contact-form']}>
-        <Form>
-          <h2>Submit a comment</h2>
-          <Form.Group controlId='formBasicEmail'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' />
-          </Form.Group>
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control type='text' placeholder='Your Name' />
+            </Form.Group>
 
-          <Form.Group controlId='formBasicPassword'>
-            <Form.Label>Name</Form.Label>
-            <Form.Control type='text' placeholder='Your Name' />
-          </Form.Group>
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                as='textarea'
+                rows='10'
+                placeholder='Leave your message here'
+              />
+            </Form.Group>
 
-          <Form.Group controlId='formBasicPassword'>
-            <Form.Label>Comment</Form.Label>
-            <Form.Control
-              as='textarea'
-              rows='10'
-              placeholder='Leave your message here'
-            />
-          </Form.Group>
-
-          <Button
-            variant='outline-light'
-            size='lg'
-            size='lg'
-            type='submit'
-            block>
-            Submit
-          </Button>
-        </Form>
+            <Button
+              variant='outline-light'
+              size='lg'
+              size='lg'
+              type='submit'
+              block>
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
